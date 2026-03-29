63e2a2d2c60f14df7d3628fa3e6191c1af0f0637
Rafal Pasko <rp.261000@gmail.com>
2026-03-29 17:18:12 +0200

    Fix least connection strategy.

--- src/strategy/least_connection.rs
+++ src/strategy/least_connection.rs
@@ -11,15 +11,15 @@ impl LoadBalancingStrategy for LeastConnectionStrategy {
 
     fn select_worker(&self, workers: &Vec<Arc<Worker>>) ->  Arc<Worker> {
          println!("{}", "Least connection is selecting worker");
-        
+
         
         workers
             .iter()
             .reduce(|a, b| {
                 if Arc::strong_count(b) > Arc::strong_count(a) {
-                    b
-                } else {
                     a
+                } else {
+                    b
                 }
             })
             .unwrap()
